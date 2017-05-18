97094c49ff195011ceec633f3382c55a8bb52596
Martin Skowronski <skowronski.martin@gmail.com>
2017-05-18 15:02:24 +0200

    fixed serializer

--- fe/app/serializers/application.js
+++ fe/app/serializers/application.js
@@ -1,30 +1,13 @@
-// import DS from 'ember-data';
-// export default DS.JSONAPISerializer.extend(DS.EmbeddedRecordsMixin,{
-//   attrs: {
-//     screenshots: { embedded: 'always' }
-//   }
-// });
-
 import DS from 'ember-data';
 import Ember from 'ember';
 import config from '../config/environment';
 
-// export default DS.JSONAPISerializer.extend(DS.EmbeddedRecordsMixin, {
 export default DS.JSONAPISerializer.extend({
-
-  modelNameFromPayloadKey: function(key) {
-  //     Ember.Logger.log("---> model", key);
-    return Ember.String.singularize(key); //this.normalizeModelName(key));
-  },
-
   payloadKeyFromModelName: function(modelName) {
       return Ember.String.singularize(modelName);
   },
 
-/*
   serialize(snapshot) {
-//     console.debug("serialize, snap", snapshot)
-//     snapshot['include'] = ['permission'];
     let serialized = this._super(...arguments);
 
     if (serialized.data.included) {
@@ -32,33 +15,40 @@ export default DS.JSONAPISerializer.extend({
       delete serialized.data.included;
     }
 
-    console.debug("serialize", serialized)
     return serialized;
   },
 
-  serializeBelongsTo(snapshot, json, relationship) {
-    let serialized = this._super(...arguments);
-
+  serializeBelongsTo(snapshot, json, relationship){
     var key = relationship.key;
-//     if (key == "permission") {
-      var belongsTo = snapshot.belongsTo(key);
-//       console.debug("serializeBelongsTo snap", snapshot);
-//       console.debug("serializeBelongsTo bel ", belongsTo);
-
-//       var js = {"type": key, "id": belongsTo.id, "attributes": belongsTo.record.toJSON() };
-      var js = this.serialize(belongsTo).data;
-      js["id"] = belongsTo.id;
-
-//       console.debug("--- serializeBelongsTo ", key, "=", belongsTo.record.toJSON());
-//       console.debug("--- serializeBelongsTo ", key, "=", js);
-
+    var belongsTo = snapshot.belongsTo(key);
+    if (belongsTo) {
+      var js = {"type": key, "id": belongsTo.id, "attributes": belongsTo.record.toJSON() };
       if (json.included) {
         json.included.push(js);
       } else {
         json["included"] = [ js ];
       }
-//       console.debug("--- serializeBelongsTo ", key, "=", json);
-//     }
+    } else {
+      console.debug("--- EMPTY: serializeBelongsTo ", key);
+    }
   },
-*/
+
+  serializeHasMany(snapshot, json, relationship){
+    var key = relationship.key;
+    var hasMany = snapshot.hasMany(key);
+    if (hasMany) {
+      var rels = [];
+        hasMany.forEach(function(snap) {
+          var js = {"type": key, "id": snap.id, "attributes": snap.record.toJSON() };
+          rels.push(js);
+      });
+      if (json.included) {
+        json.included.concat(rels);
+      } else {
+        json["included"] = rels;
+      }
+    } else {
+      console.debug("--- EMPTY: serializeHasMany ", key);
+    }
+  },
 });
