dcb9ef2ccfb27481c77a4b5fdaa3c71906bcc174
Martin Skowronski <skowronski.martin@gmail.com>
2017-05-19 20:15:39 +0200

    cleanup

--- fe/app/router.js
+++ fe/app/router.js
@@ -9,12 +9,12 @@ const Router = Ember.Router.extend({
 Router.map(function() {
     this.route('login');
 
-    this.route('overview', { resetNamespace: true }, function() {
+    this.route('overview', function() {
       this.route('unit', { path: '/:unit_id' }, function() {
       });
     });
 
-    this.route('players', { resetNamespace: true }, function() {
+    this.route('players', function() {
       this.route('player', { path: '/:player_id' }, function() {
         this.route('general');
         this.route('stats');
@@ -22,12 +22,31 @@ Router.map(function() {
       });
     });
 
-    this.route('users', { resetNamespace: true }, function() {
+    this.route('users', function() {
       this.route('user', { path: '/:user_id' }, function() {
       });
     });
 
     this.route('items');
+    /*
+    this.route('items', function() {
+      this.route('categories', function() {
+        this.route('cayegory', { path: '/:category_id' }, function() {
+        });
+      });
+
+      this.route('templates', function() {
+        this.route('template', { path: '/:template_id' }, function() {
+        });
+      });
+
+      this.route('items', function() {
+        this.route('item', { path: '/:item_id' }, function() {
+        });
+      });
+    });
+*/
+
     this.route('rewards');
 
     this.route('lfg');
