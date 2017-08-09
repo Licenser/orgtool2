fc0076be6ee904946bde60b26170f1ed63572695
Martin Skowronski <skowronski.martin@gmail.com>
2017-08-09 16:19:01 +0200

    fixed user filter and reload issue

--- fe/app/users/controller.js
+++ fe/app/users/controller.js
@@ -13,13 +13,9 @@ export default Ember.Controller.extend({
   itemHeight: 42,
 
 /////////////////////////////////////
-//
-//
+
   hasParent: function(id, unit) {
     try {
-//       if (unit.get("id") == 102) {
-//         Ember.Logger.debug("wtf now again?", unit.get("id"), " - ", unit.get("parent"), " = ", (unit.get("parent") ? unit.get("parent").get("isLoaded") : "-")); 
-//       }
       return (unit && unit.get("id") == id) || (unit && unit.get('parent') && this.hasParent(id, unit.get('parent')));
     } catch(err) {
         Ember.Logger.debug("error", err);
@@ -27,95 +23,27 @@ export default Ember.Controller.extend({
     return false;
   },
 
-
-  filteredContent: Ember.computed.filter('players', function(player, index, array) {
+  filteredContent: Ember.computed.filter('model', function(user, index, array) {
     var searchFilter = this.get('searchFilter');
-    var unitFilter = this.get('unitFilter');
-    var res = []
-
-    if (Ember.isEmpty(searchFilter) && Ember.isEmpty(unitFilter)) {
+    if (Ember.isEmpty(searchFilter)) {
       return true;
     }
 
-    if (!Ember.isEmpty(searchFilter)) {
-      var regex = new RegExp(searchFilter, 'i');
-
-      if (get(player, "name").match(regex)) {
-        return true;
-      }
-
-      var handles;
-      var all = get(player, 'handles');
-      for (var i = 0; i < get(all, 'length'); i++) {
-        if (all.objectAt(i).get("handle").match(regex)) {
-          return true;
-        }
-      }
-
-      return false;
+    var regex = new RegExp(searchFilter, 'i');
+    if (get(user, "name").match(regex) || get(user, "email").match(regex)) {
+      return true;
     }
 
-    if (!Ember.isEmpty(unitFilter)) {
-      var self = this;
-      res = player.get('units').filter(function(unit, index, enumerable){
-        return self.hasParent(unitFilter.get("id"), unit.id);
-      });
-      if (Ember.isEmpty(res)) {
-        return false;
-      }
-    }
-
-    return res;
-  }).property('searchFilter', 'players.length', 'unitFilter'),
+    return false;
+  }).property('model.length', 'searchFilter'),
 
   sortedContent: Ember.computed.sort('filteredContent', 'sortProperties').property('filteredContent'),
 
 
-
-//////////////////////////////////////
-
-
-  deleteMember: function(player) {
-      if (!player) {
-        return;
-      }
-      Ember.Logger.debug("delete user");
-//       player.deleteRecord('player'); //this.store.createRecord('player');
-      var self = this;
-      player.destroyRecord().then(function(done) {
-        self.transitionToRoute('players');
-      }).catch(function(err) {
-        Ember.Logger.debug("delete  user", err);
-      });
-//       this.set('searchFilter', '');
-
-  },
-
-
   actions: {
-    createMember: function() {
-        Ember.Logger.debug("create user");
-        var player = this.get('store').createRecord('player'); //this.store.createRecord('player');
-        var self = this;
-        player.save({include: ["permission"]}).then(function(done) {
-          self.transitionToRoute('players.player', done.get('id'));
-        }).catch(function(err) {
-          Ember.Logger.debug("create user", err);
-        });
-  //       this.set('searchFilter', '');
-
-    },
-
-    setUnitFilter: function(data) {
-//       Ember.Logger.debug("set ", data);
-      set(this, 'unitFilter', data);
-    },
-
     clearFilter: function() {
-//       Ember.Logger.debug("clear");
       this.set('searchFilter', '');
     },
-
   }
 
 });
--- fe/app/users/route.js
+++ fe/app/users/route.js
@@ -7,4 +7,9 @@ export default Ember.Route.extend({
   model: function() {
     return this.store.findAll("user");
   },
+  afterModel: function(model, transition) {
+    model.forEach(function(user) {
+      user.reload();
+    });
+  },
 });
