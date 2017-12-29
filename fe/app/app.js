21e7c0617d9ba844225a312c9bda951013b4b2ae
Martin Skowronski <skowronski.martin@gmail.com>
2017-12-29 23:58:19 +0100

    added change list to confirm dialog

--- fe/app/app.js
+++ fe/app/app.js
@@ -2,6 +2,33 @@ import Ember from 'ember';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
+import DS from 'ember-data';
+
+DS.Model.reopen({
+
+  // this could break at the drop of a hat, so we'll want to test it thoroughly
+  changedAttributes() {
+    const attributes = this._super(...arguments)
+    const relationships = {}
+
+    // check relationships
+    this.eachRelationship((name, meta) => {
+      if (meta.kind === 'belongsTo') {
+
+        let before = this.get(`_internalModel._relationships.initializedRelationships.${name}.canonicalState.id`)
+        let now = this.get(`${name}.id`)
+
+        if (before !== now) {
+          relationships[name] = [before, now]
+        }
+      }
+    })
+
+    return Ember.merge(attributes, relationships)
+  },
+
+});
+
 
 const App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
--- fe/app/components/confirm-dialog/component.js
+++ fe/app/components/confirm-dialog/component.js
@@ -13,6 +13,32 @@ export default Ember.Component.extend({
 //     Ember.Logger.debug(">>> msg", msg);
 //   }),
 
+  capitalizeFirstLetter: function(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  },
+
+  checkLength: function(val) {
+//     if (typeof val === 'string' || val instanceof String) {
+//       return val.length > 30 ? "no long" : val;
+//     }
+    return val;
+  },
+
+  changelist: Ember.computed("msg.item", function() {
+    var ret = Ember.A()
+    if (get(this, "msg.item")) {
+      var cs = get(this, "msg.item").changedAttributes();
+      var keys = Object.keys(cs);
+      for (var key in cs) {
+        if (Array.isArray(cs[key]) && cs[key].length == 2) {
+          ret.pushObject({name: this.capitalizeFirstLetter(key), was: this.checkLength(cs[key][0]), is: this.checkLength(cs[key][1])});
+        } else {
+          console.debug(">>>> KEY not handed", key, cs[key]);
+        }
+     }
+    }
+    return ret;
+  }),
 
   actions: {
     close: function() {
